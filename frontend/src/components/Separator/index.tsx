ff9718cdd775f0a507536a84f5a55ab15c202280
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-08-28 16:18:53 +0200

    feat: create accessible separator

---
+++ frontend/src/components/Separator/index.tsx
@@ -0,0 +1,5 @@
+"use client";
+import * as S from "./styles";
+export default function Separator() {
+  return <S.Separator />;
+}
---
+++ frontend/src/components/Separator/styles.tsx
@@ -0,0 +1,9 @@
+"use client";
+import * as RadixSeparator from "@radix-ui/react-separator";
+import { styled } from "styled-components";
+
+export const Separator = styled(RadixSeparator.Root)`
+  background-color: var(--dark-gray);
+  height: 1px;
+  width: 100%;
+`;
